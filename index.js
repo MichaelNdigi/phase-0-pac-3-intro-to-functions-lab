3687e6058aa466b988e12e0737c8440ce544d87a
Michael Ndigi <michael.ndigi@student.moringaschool.com>
2022-08-03 14:53:17 +0300

    Assignment completed

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase();
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+"HELLO!".toLowerCase();
+
+ function logShout(string){
+    console.log(string.toUpperCase());
+ }
+"hello".toUpperCase;
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+ }
+"HELLO".toLowerCase;
+
+function sayHiToHeadphonedRoommate(string){
+    if(string.toLowerCase() === string){
+        
+        return "I can't hear you!";
+    }
+    if(string.toUpperCase() === string){
+        
+        return "YES INDEED!";
+    }
+    if(string === "Let's have dinner together!"){
+        
+        return "I would love to!";
+    }
+}
+
+
